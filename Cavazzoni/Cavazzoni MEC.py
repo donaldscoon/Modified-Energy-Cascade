19b66a5471c4abbe54459251c4fdb0d3952d9ab0
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-02-06 12:38:53 -0500

    Create Cavazzoni MEC.py

---
+++ Cavazzoni/Cavazzoni MEC.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 12:33:34 2023
+
+@author: donal
+
+Written to recreate the MEC authored by Jones and Cavazzoni. Created using the 
+Life Support Baseline Values and Assumptions Document by Ewert 2020
+
+End goal is global sensitivity and uncertainty analysis of this version and comparison against others. 
+
+"""
+
+
