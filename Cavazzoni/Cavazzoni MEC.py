7478492849d9bad00bc0a280cb621f79125ad5e6
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-02-06 13:19:04 -0500

    Started the t_A equations

--- Cavazzoni/Cavazzoni MEC.py
+++ Cavazzoni/Cavazzoni MEC.py
@@ -11,4 +11,62 @@ End goal is global sensitivity and uncertainty analysis of this version and comp
 
 """
 
+##################################################
+################## MODEL INPUTS ##################
+##################################################
+PPFD = 800          # umol/m^2/sec, needs to accept inputs
+CO2 = 450           # umol CO2 / mol air,needs to accept  inputs
 
+##################################################
+############## INTIALIZING VARIABLES  ############
+##################################################
+t = 0               # time in days
+n = 2.5             # Ewert table 4-97 crop specific
+
+
+##################################################
+############# SUPPLEMENTAL EQUATIONS #############
+##################################################
+""" Canopy Closure t_A """
+# tac values originate from EWert table 4-115 
+tac1 = 0
+tac2 = 1.0289*10**4
+tac3 = -3.7018
+tac4 = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(tac2)
+t_A_1 = 2
+t_A_2 = 4
+
+t_A = t_A_1 + t_A_2
+
+##################################################
+################# THE MODEL LOOP #################
+##################################################
+while t <= 100:     
+    
+    t += 1
+    
+print(t)
