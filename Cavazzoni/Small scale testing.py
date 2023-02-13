7035e7fc8b96cbe4487e2609cd32e160addf9f25
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-02-13 13:57:58 -0500

    SST figured out dataframe structure

--- Cavazzoni/Small scale testing.py
+++ Cavazzoni/Small scale testing.py
@@ -1,14 +1,4 @@
 import numpy as np
 
-t = 0
-dt = 1
-harvest = 10
-test_mat = np.zeros(harvest)
-print(test_mat)
-TEB = 0
-for t in range(harvest):
-    t += dt
-    TEB = t*16
-    test_mat[t] = TEB
-    print(test_mat)
-    print(t)
+DTR = 3600*16
+print(DTR)
