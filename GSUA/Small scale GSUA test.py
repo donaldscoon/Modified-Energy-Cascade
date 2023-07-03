019b46d7d725fde3183051b894b2f4df11e44a99
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-07-03 09:49:30 -0400

    model lists of lists added

--- GSUA/Small scale GSUA test.py
+++ GSUA/Small scale GSUA test.py
@@ -24,14 +24,6 @@ problem = {
                [0,24]]        # Photoperiod
                }
 
-mec_outputs = ["CQY","CUE_24","DCG",
-               "CGR","TCB","TEB",
-               "VP_SAT","VP_AIR","VPD",
-               "P_GROSS","P_NET","g_S",
-               "g_A","g_C","DTR",
-               "T_LIGHT","T_DARK","RH",
-               "CO2"]
-
 ##########################################################
 ############## Generate the Samples ######################
 ##########################################################
@@ -93,38 +85,77 @@ Y = np.loadtxt('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSU
 #################### VISUALIZATIONS #######################
 ###########################################################
 
-"""This chart bulding stuff works!"""
-VIS_GSUA = df_CAV_sims[['Simulation', 'DTR', 'T_LIGHT', 'RH', 'CO2']]
-VIS_GSUA = VIS_GSUA.sort_values('RH', ascending=True)
-x = VIS_GSUA[['RH']]
-x = x.values.flatten()
-y = VIS_GSUA[['DTR']]
-y = y.values.flatten()
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-ax.set_ylabel('Daily Transpiration Rate')
-ax.set_xlabel('Relative Humidity')
+u = "\u00B5"        # unicode for the micro symbol
+# This list of lists defines the short name, full name and units for the inputs/outputs used in charts.
+mec_labels = [  
+            ["A", "Absorption", ""],
+            ["CQY", "Canopy Quantum Yield", u+"mol$_{fixed}$ "+u+"mol$_{aborbed}$"],
+            ["CUE_24", "Carbon Use Efficiency", ""],
+            ["DCG", "Daily Carbon Gain", "mol$_{carbon}$ m$^{-2}$ day$^{-1}$"],
+            ["CGR", "Crop Growth Rate", "grams m$^{-2}$ day$^{-1}$"],
+            ["TCB", "Total Crop Biomass", "grams m$^{-2}$"],
+            ["TEB", "Total Edible Biomass", "grams m$^{-2}$"],
+            ["VP_SAT", "Saturated Moisture Vapor Pressure", "kPa"],
+            ["VP_AIR", "Actual Moisture Vapor Pressure", "kPa"],
+            ["VPD", "Vapor Pressure Deficit", "kPa"],
+            ["P_GROSS", "Gross Canopy Photosynthesis", u+"mol$_{carbon}$ m$^{-2}$ second$^{-1}$"],
+            ["P_NET", "Net Canopy Photosynthesis", u+"mol$_{carbon}$ m$^{-2}$ second$^{-1}$"],
+            ["g_S", "Stomatal Conductance", "mol$_{water}$ m$^{-2}$ second$^{-1}$"],
+            ["g_A", "Atmospheric Conductance", "mol$_{water}$ m$^{-2}$ second$^{-1}$"],
+            ["g_C", "Canopy Conductance", "mol$_{water}$ m$^{-2}$ second$^{-1}$"],
+            ["DTR", "Daily Tranpiration Rate", "L$_{water}$ m$^{-2}$ day$^{-1}$"],
+            ["T_LIGHT", "Light Cycle Temperature", "Degrees Celsius"],
+            ["T_DARK", "Dark Cycle Temperature", "Degrees Celsius"],
+            ["RH", "Relative Humidity", "%"],
+            ["CO2", "CO$_{2}$ Concentration", u+"mol$_{carbon}$ mol$_{air}$"]
+            ]
 
-# plt.axhline(y=np.nanmean(y), color='red', linestyle='--', linewidth=3, label='Avg')     # just the straight average of the DTR for all simulations
-
-# calc the trendline
-z = np.polyfit(x, y, 1) # 1 is linear, 2 is quadratic! the flatten converts the df to a 1D array
-p = np.poly1d(z)
-plt.plot(x,p(x),"red")
-
-# fit a linear curve and estimate its y-values and their error.
-a, b = np.polyfit(x, y, deg=1)
-y_err = x.std() * np.sqrt(1/len(x) + (x - x.mean())**2 / np.sum((x - x.mean())**2))
-
-# trying to add a shaded region to represent XX quantity of the simulations
-fig, ax = plt.subplots()
-ax.plot(x, y, '-')
-ax.fill_between(x, y - y_err, y + y_err, alpha=0.2)
-ax.plot(x, y, 'o', color='tab:brown')
+for label in mec_labels:
+    short_name = label[0]
+    long_name = label[1]
+    unit = label[2]
+    # print(short, long, unit)
+    fig, ax = plt.subplots()
+    ax.set_xlabel(short_name, fontsize= 14)
+    ax.set_ylabel(unit, fontsize= 14)
+    ax.tick_params(labelsize= 12)
+    plt.title(long_name, fontsize= 16)
+    plt.show()
 
 
-# plt.plot(x,p(x)*(1.95),"green")
-# plt.plot(x,p(x)*(.05),"green")
-# the line equation:
-print("y=%fx+(%f)"%(z[0],z[1]))
-plt.show()
+
+# """This chart bulding stuff works!"""
+# VIS_GSUA = df_CAV_sims[['Simulation', 'DTR', 'T_LIGHT', 'RH', 'CO2']]
+# VIS_GSUA = VIS_GSUA.sort_values('RH', ascending=True)
+# x = VIS_GSUA[['RH']]
+# x = x.values.flatten()
+# y = VIS_GSUA[['DTR']]
+# y = y.values.flatten()
+# fig, ax = plt.subplots()
+# ax.scatter(x, y)
+# ax.set_ylabel('Daily Transpiration Rate')
+# ax.set_xlabel('Relative Humidity')
+
+# # plt.axhline(y=np.nanmean(y), color='red', linestyle='--', linewidth=3, label='Avg')     # just the straight average of the DTR for all simulations
+
+# # calc the trendline
+# z = np.polyfit(x, y, 1) # 1 is linear, 2 is quadratic! the flatten converts the df to a 1D array
+# p = np.poly1d(z)
+# plt.plot(x,p(x),"red")
+
+# # fit a linear curve and estimate its y-values and their error.
+# a, b = np.polyfit(x, y, deg=1)
+# y_err = x.std() * np.sqrt(1/len(x) + (x - x.mean())**2 / np.sum((x - x.mean())**2))
+
+# # trying to add a shaded region to represent XX quantity of the simulations
+# fig, ax = plt.subplots()
+# ax.plot(x, y, '-')
+# ax.fill_between(x, y - y_err, y + y_err, alpha=0.2)
+# ax.plot(x, y, 'o', color='tab:brown')
+
+
+# # plt.plot(x,p(x)*(1.95),"green")
+# # plt.plot(x,p(x)*(.05),"green")
+# # the line equation:
+# print("y=%fx+(%f)"%(z[0],z[1]))
+# plt.show()
