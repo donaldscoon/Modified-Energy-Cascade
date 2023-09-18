183367b7e99142bb4f6e46444b943fcf2a17fe4c
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-09-18 09:44:19 -0400

    EE Output acheived!!!

--- GSUA/MorrisEEtest.py
+++ GSUA/MorrisEEtest.py
@@ -2,11 +2,15 @@ from SALib.sample import saltelli
 from SALib.analyze import sobol
 from SALib.sample import morris
 from SALib import ProblemSpec
+from datetime import datetime
+from SALib.test_functions import Ishigami
+
+
 import SALib as SALib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
+
 
 models = [
          ["AMI", "Amitrano"], 
@@ -57,25 +61,8 @@ mec_inputs = [
             ["H", "Photoperiod", "hours day$^{-1}$"]
 ]
 
-sp = ProblemSpec({
-    'names': ['TEMP', 'RH', 'CO2', 'PPFD', 'H'],
-    # Notation [min, max, peak as % of that range]
-    # for example, with  Temp the range is 35. 54.286% of that would equal 19, 
-    # which would actually give a peak at 24 since the range starts at 5.
-    'bounds': [[5,40,0.54286],      # Temperature Peak at 24 
-               [35,100,0.38461],    # Relative Humidity Peak at 60
-               [330,1300,0.48453],  # Atmo CO2 Concentration Peak at 800
-               [0,1100,0.27273],    # PPFD Level Peak at 300
-               [0,24, 0.66667]],    # Photoperiod Peak at 16
-    'dists': ['triang',             # Temperature
-              'triang',             # Relative Humidity
-              'triang',             # Atmo CO2
-              'triang',             # PPFD
-              'triang'],            # Photoperiod
-    'outputs': ['Y']
-})
 
-problem = {
+problem = ProblemSpec({
     'names': ['TEMP', 'RH', 'CO2', 'PPFD', 'H'],
     'num_vars': 5,
     'bounds': [[5,40,0.54286],      # Temperature Peak at 24 
@@ -83,21 +70,21 @@ problem = {
                [330,1300,0.48453],  # Atmo CO2 Concentration Peak at 800
                [0,1100,0.27273],    # PPFD Level Peak at 300
                [0,24, 0.66667]],    # Photoperiod Peak at 16
-    'dists': ['triang', 'triang', 'triang', 'triang', 'triang']
-    }
+    'dists': ['triang', 'triang', 'triang', 'triang', 'triang'],
+    # 'groups': None
+    })
 
 ###########################################################
 #################### Analysis #############################
 ###########################################################
-print("Beginning Analysis of simulations")
 
 # Create dataframes for each models GSUA runs
 df_AMI_sims = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_AMI_out/data/GSUA_AMI_Simulations.csv')
 df_BOS_sims = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_BOS_out/data/GSUA_BOS_Simulations.csv')
 df_CAV_sims = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_CAV_out/data/GSUA_CAV_Simulations.csv')
 
-X = morris.sample(sp, 768, num_levels=2)
-# print(X)
+X = morris.sample(problem, 128, num_levels=4) # this is set to 128 so the array = 768 to match the ourputs of the model
+
 for item in models:                 # loop for model names
     model_short_name = item[0]
     model_long_name = item[1]
@@ -105,10 +92,7 @@ for item in models:                 # loop for model names
         output_short_name = item[0]
         output_long_name = item[1]
         output_unit = item[2]
-        # Loading specific outputs for GSUA analysis 
+        # Loading specific outputs for Morris EE analysis 
+        print(problem)
         Y = np.loadtxt(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_{model_short_name}_out/data/GSUA_{model_short_name}_data_{output_short_name}.txt') # done to match the SALib example, imports the text file result
-        # print(Y)
-        sp.set_results(Y)
-
-        Si = SALib.analyze.morris.analyze(sp, X, Y, conf_level=0.95, print_to_console=True, num_levels=2)
-        print(Si)
+        Si = SALib.analyze.morris.analyze(problem, X, Y, conf_level=0.95, print_to_console=True, num_levels=4)
