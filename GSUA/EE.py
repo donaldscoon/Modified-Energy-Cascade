58fec99b3d61ed509f0a284b64687245eae83575
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-09-20 12:40:08 -0400

    EE chart 2 draft complete?

--- GSUA/EE.py
+++ GSUA/EE.py
@@ -88,6 +88,7 @@ problem = ProblemSpec({
 #     df_BOS_sims = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_BOS_out/data/GSUA_BOS_Simulations.csv')
 #     df_CAV_sims = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_CAV_out/data/GSUA_CAV_Simulations.csv')
 
+#     N = 128
 #     X = np.loadtxt('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/SOBOL_parameters.txt')
 
 #     EE_out_df = pd.DataFrame()
@@ -101,7 +102,7 @@ problem = ProblemSpec({
 #             output_unit = item[2]
 #             # Loading specific outputs for Morris EE analysis 
 #             Y = np.loadtxt(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/GSUA_{model_short_name}_out/data/GSUA_{model_short_name}_data_{output_short_name}.txt') # done to match the SALib example, imports the text file result
-#             EE = SALib.analyze.morris.analyze(problem, X, Y, conf_level=0.95, num_levels=128) # analyzes the Elementary effects for each models ouput
+#             EE = SALib.analyze.morris.analyze(problem, X, Y, conf_level=0.95, num_levels=N) # analyzes the Elementary effects for each models ouput
 
 #             with open(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/results/full_out/{model_short_name}_{output_short_name}_EE_results.txt', 'w') as f:
 #                 results_df = EE.to_df()
@@ -150,13 +151,75 @@ def CHART():
     # read in the data
     EE_out_df = pd.read_csv('C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/results/EE_out.csv')
 
-    # mu star by sigma with a 1/1 line
+    # '''##########################################################
+    #  mu star by sigma with a 1/1 line
+    #  #########################################################'''
+    # for item in mec_outputs:   # loop for outputs
+    #     output_short_name = item[0]
+    #     output_long_name = item[1]
+    #     output_unit = item[2]
+
+    #     plt.figure() # creates the figure for each output type, which is then iterated by model.
+    #     for item in models:                 # loop for model names
+    #         model_short_name = item[0]
+    #         model_long_name = item[1]
+
+    #         # sets the color of each model for that loop.
+    #         if model_short_name == 'AMI':
+    #             color = ami_c
+    #         elif model_short_name == 'BOS':
+    #             color = bos_c
+    #         elif model_short_name == 'CAV':
+    #             color = cav_c
+    #         # print(model_short_name, output_short_name)
+    #         X = EE_out_df[f'{model_short_name}_{output_short_name}_mu_star']
+    #         Y = EE_out_df[f'{model_short_name}_{output_short_name}_sigma']
+
+    #         # Check if both X and Y have all zero values
+    #         if not all(x == 0 for x in X) or not all(y == 0 for y in Y):
+                    
+    #             # define which points correspond to which inputs
+    #             mu_star_temp = X[0]
+    #             mu_star_rh   = X[1]
+    #             mu_star_CO2  = X[2]
+    #             mu_star_PPFD = X[3]
+    #             mu_star_H    = X[4]
+
+    #             sigma_temp   = Y[0]
+    #             sigma_rh     = Y[1]
+    #             sigma_CO2    = Y[2]
+    #             sigma_PPFD   = Y[3]
+    #             sigma_H      = Y[4]
+
+    #             # Create the scatter plot
+    #             plt.scatter(mu_star_temp, sigma_temp,    s=50, marker= 'o', color= color, label= f"{model_short_name}_TEMP")
+    #             plt.scatter(mu_star_rh, sigma_rh,        s=50, marker= 's', color= color, label= f"{model_short_name}_RH")
+    #             plt.scatter(mu_star_CO2, sigma_CO2,      s=50, marker= '*', color= color, label= f"{model_short_name}_CO2")
+    #             plt.scatter(mu_star_PPFD, sigma_PPFD,    s=50, marker= '^', color= color, label= f"{model_short_name}_PPFD")
+    #             plt.scatter(mu_star_H, sigma_H,          s=50, marker= 'd', color= color, label= f"{model_short_name}_H")
+
+    #             # Add a 1:1 line
+    #             min_val = min(min(X), min(Y))
+    #             max_val = max(max(X), max(Y))
+    #             plt.plot([min_val, max_val], [min_val, max_val], color='gray', linestyle='-')
+
+    #     # Set the labels and title
+    #     plt.xlabel('mu*')
+    #     plt.ylabel('sigma')
+    #     plt.title(f'EE of {output_short_name}')
+    #     plt.legend()
+    #     # plt.show()
+    #     plt.savefig(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/figures/Elementary_Effects/EE_1-1_{output_short_name}.png', bbox_inches='tight') #there are many options for savefig
+
+    '''###############################################################################
+    mu by sigma with the V
+    ###############################################################################'''
+
     for item in mec_outputs:   # loop for outputs
         output_short_name = item[0]
         output_long_name = item[1]
         output_unit = item[2]
-
-        plt.figure() # creates the figure for each output type, which is then iterated by model.
+        fig, ax = plt.subplots() # creates the figure for each output type, which is then iterated by model.
         for item in models:                 # loop for model names
             model_short_name = item[0]
             model_long_name = item[1]
@@ -169,18 +232,18 @@ def CHART():
             elif model_short_name == 'CAV':
                 color = cav_c
             # print(model_short_name, output_short_name)
-            X = EE_out_df[f'{model_short_name}_{output_short_name}_mu_star']
+            X = EE_out_df[f'{model_short_name}_{output_short_name}_mu']
             Y = EE_out_df[f'{model_short_name}_{output_short_name}_sigma']
 
             # Check if both X and Y have all zero values
             if not all(x == 0 for x in X) or not all(y == 0 for y in Y):
                     
                 # define which points correspond to which inputs
-                mu_star_temp = X[0]
-                mu_star_rh   = X[1]
-                mu_star_CO2  = X[2]
-                mu_star_PPFD = X[3]
-                mu_star_H    = X[4]
+                mu_temp = X[0]
+                mu_rh   = X[1]
+                mu_CO2  = X[2]
+                mu_PPFD = X[3]
+                mu_H    = X[4]
 
                 sigma_temp   = Y[0]
                 sigma_rh     = Y[1]
@@ -189,27 +252,71 @@ def CHART():
                 sigma_H      = Y[4]
 
                 # Create the scatter plot
-                plt.scatter(mu_star_temp, sigma_temp,    s=50, marker= 'o', color= color, label= f"{model_short_name}_TEMP")
-                plt.scatter(mu_star_rh, sigma_rh,        s=50, marker= 's', color= color, label= f"{model_short_name}_RH")
-                plt.scatter(mu_star_CO2, sigma_CO2,      s=50, marker= '*', color= color, label= f"{model_short_name}_CO2")
-                plt.scatter(mu_star_PPFD, sigma_PPFD,    s=50, marker= '^', color= color, label= f"{model_short_name}_PPFD")
-                plt.scatter(mu_star_H, sigma_H,          s=50, marker= 'd', color= color, label= f"{model_short_name}_H")
+                plt.scatter(mu_temp, sigma_temp,    s=50, marker= 'o', color= color, label= f"{model_short_name}_TEMP")
+                plt.scatter(mu_rh, sigma_rh,        s=50, marker= 's', color= color, label= f"{model_short_name}_RH")
+                plt.scatter(mu_CO2, sigma_CO2,      s=50, marker= '*', color= color, label= f"{model_short_name}_CO2")
+                plt.scatter(mu_PPFD, sigma_PPFD,    s=50, marker= '^', color= color, label= f"{model_short_name}_PPFD")
+                plt.scatter(mu_H, sigma_H,          s=50, marker= 'd', color= color, label= f"{model_short_name}_H")
 
-                # Add a 1:1 line
-                min_val = min(min(X), min(Y))
-                max_val = max(max(X), max(Y))
-                plt.plot([min_val, max_val], [min_val, max_val], color='gray', linestyle='-')
+                # Add +-2SEM LINES
+                # calc SD of mu, 
+                mu_sd = EE_out_df[f'{model_short_name}_{output_short_name}_mu'].std()
+                # calc sqrt of N, 
+                denom = np.emath.sqrt(128)
+                pos2_SEM = 2*(mu_sd/denom)
+                neg2_SEM = -2*(mu_sd/denom)
+                # I still don't fully understand how this centers the x axis to 0 but it does!
+                x_max = np.abs(ax.get_xlim()).max()
+                ax.set_xlim(xmin=-x_max, xmax=x_max)
+                # print(model_short_name, output_short_name)
+                # print(pos2_SEM, neg2_SEM)
+                # Add lines from (0, 0) to SEM values
+                plt.plot([0, neg2_SEM], [0, max(Y)], color=color, linestyle='--', label='-2SEM Line')
+                plt.plot([0, pos2_SEM], [0, max(Y)], color=color, linestyle='--', label='+2SEM Line')
+
+
+                # plt.plot([0, pos2_SEM], [0, pos2_SEM], color='gray', linestyle='-')
+                # plt.plot([neg2_SEM, 0], [neg2_SEM, 0], color='gray', linestyle='-')
+
+                #plot against mu
+                # print(X[0], pd.Series(Y[0]))
 
         # Set the labels and title
-        plt.xlabel('mu*')
+        plt.xlabel('mu')
         plt.ylabel('sigma')
         plt.title(f'EE of {output_short_name}')
         plt.legend()
-        # plt.show()
-        plt.savefig(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/figures/Elementary_Effects/EE_1-1_{output_short_name}.png', bbox_inches='tight') #there are many options for savefig
+        plt.show()
+        # plt.savefig(f'C:/Users/donal/Documents/GitHub/Modified-Energy-Cascade/GSUA/figures/Elementary_Effects/EE_1-1_{output_short_name}.png', bbox_inches='tight') #there are many options for savefig
 
 
-    # mu by sigma with the V
 # Executes this program/function
 if __name__ ==('__main__'):
     CHART()
+
+""" NOTES FROM THE MATLAB CODE `EE_SENMAE_CALC.M`
+
+Two figures/subplots are produced for each model output.    %
+%             The first subplot is a plot of µ* - ? while the second      %
+%             one consists of µ - ?.                                      %
+%                                                                         %
+%             We plot 1:1 threshold(dotted black)in the first             %
+%             subplot and µ = +/-2 ?/sqrt(r)threshold lines               %
+%             (dotted black)in the second subplot. µ = +/-2 ?/sqrt(r)     %
+%             lines were proposed by Morris (1991)to identify factor with %
+%             dominant non-additive/non-linear effects. 1:1 (i.e. µ*=?)   %
+%             line threshold is based on Khare et al.(2019)               %
+%                                                                         %
+%             Additionally, factors for which µ* = abs(µ) holds true i.e. %
+%             perfectly monotonic effects are indicated by solid red circles%
+%             while rest are indicated by blue asterisk. Also, bootstrapping %
+%             95% CI for µ* and µ are indicated (golden color) on         %
+%             respective subplots.                                        %
+%                                                                         %
+%             Plots are automatically saved as PDF files.                 %
+%                                                                         %
+% References: Khare et al. (2019), Effective Global Sensitivity Analysis  %
+%             of a High-Dimensional Hydrologic and Water Quality Models.  %
+%             Journal of Hydrologic Engineering, 24(1).                   %
+%             DOI: 10.1061/(ASCE)HE.1943-5584.0001726. 
+"""
