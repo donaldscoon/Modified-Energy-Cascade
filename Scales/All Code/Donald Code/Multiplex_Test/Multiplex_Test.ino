7430ce79330c0d862a49b7c4d52d8188c0dd3a37
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2023-10-26 10:20:23 -0400

    IDK why it keeps restarting the setup loop.

--- Scales/All Code/Donald Code/Multiplex_Test/Multiplex_Test.ino
+++ Scales/All Code/Donald Code/Multiplex_Test/Multiplex_Test.ino
@@ -13,10 +13,10 @@ HX711 scale2;
 HX711 scale3;
 HX711 scale4;
 
+// HX711 scales[4];  // Array of HX711 objects
 //////////////////////////////////////////////////////////////////////////////////////////
 int timer = 1000;                   // 1000 milliseconds
 int scale_pins[] = {7, 8, 9, 10};   // an array of pin numbers to which scales are attached
-const char* scale_num[] = {"scale1", "scale2", "scale3", "scale4"};  // an array of scale names, which I can hopefully use?
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -26,7 +26,13 @@ void setup() {
   Serial.begin(9600);
 
 //////////////////////////////////////////////////////////////////////////////////////////
+  // for (int i = 0; i < 4; i++) {
+  //   scales[i].begin(DOUT, CLK);
 
+  //   scales[i].set_scale(getScaleFactor(i));
+
+  //   scales[i].tare(); 
+  // }
   scale1.begin(DOUT, CLK);
   scale2.begin(DOUT, CLK);
   scale3.begin(DOUT, CLK);
@@ -37,21 +43,13 @@ void setup() {
   scale3.set_scale(466.98718);//This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale4.set_scale(498.09172);//This value is obtained by using the SparkFun_HX711_Calibration sketch
 
-  // int i = 0;
-  // // this loop iterates throught the list of pins used to control the mosfets
-  // // It selects a pin, turn it on (LOW POWER), waits a second, tares the scale, 
-  // // shuts down, and waits a second before moving to the next pin/scale.
-  // while (i < sizeof(scale_pins) / sizeof(scale_pins[0])) { 
-
-  //   pinMode(scale_pins[i], OUTPUT);
-  //   digitalWrite(scale_pins[i], LOW);
-
-  //   // Need to fix this! SEE OPERA ARIA!!!
-  //   i++;
   scale1.tare();
   scale2.tare();
   scale3.tare();
   scale4.tare();
+
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 
   //setTime(hour, minute, second, day, month, year), Set time for when program starts and time is set to 24hr loop. There is also a 4 sec delay.
   setTime(18, 18, 0, 11, 9, 2023);
@@ -68,63 +66,86 @@ void loop() {
   // Calculate the approximate UTC time
   time_t utcTime = localTime - timeZoneOffset;
   
-  Serial.print(year(utcTime)); Serial.print("-");
-  Serial.print(month(utcTime)); Serial.print("-");
-  Serial.print(day(utcTime)); Serial.print(" ");  
-  Serial.print(hour(utcTime)); Serial.print(":");
-  Serial.print(minute(utcTime)); Serial.print(":");
-  Serial.print(second(utcTime)); Serial.print(";");
+  // Serial.print(year(utcTime)); Serial.print("-");
+  // Serial.print(month(utcTime)); Serial.print("-");
+  // Serial.print(day(utcTime)); Serial.print(" ");  
+  // Serial.print(hour(utcTime)); Serial.print(":");
+  // Serial.print(minute(utcTime)); Serial.print(":");
+  // Serial.print(second(utcTime)); Serial.print(";");
 
 //////////////////////////////////////////////////////////////////////////////////////////
-  // loop from the lowest pin to the highest:
-  int i = 0;
-  // this loop iterates throught the list of pins used to control the mosfets
-  // It selects a pin, turn it on (LOW POWER), waits a second, takes a reading, 
-  // shuts down, and waits a second before moving to the next pin/scale.
-  while (i < sizeof(scale_pins) / sizeof(scale_pins[0])) { 
-    // Pin Identification Debugger
-    Serial.print("The Pin is ");
-    Serial.print(scale_pins[i]);
-    Serial.print(";");
+  // // loop from the lowest pin to the highest:
+  // int i = 0;
+  // // this loop iterates throught the list of pins used to control the mosfets
+  // // It selects a pin, turn it on (LOW POWER), waits a second, takes a reading, 
+  // // shuts down, and waits a second before moving to the next pin/scale.
+  // while (i < sizeof(scale_pins) / sizeof(scale_pins[0])) { 
+  // //   // Pin Identification Debugger
+  // //   Serial.print("Pin: ");
+  // //   Serial.print(scale_pins[i]);
+  // //   Serial.print(";");
     
-    // Scale Identification Debugger
-    Serial.print("The Scale is ");
-    Serial.print(scale_num[i]);
-    Serial.print(";");
+  // //   // Scale Identification Debugger
+  // //   Serial.print("Scale: ");
+  // //   Serial.print(i);
+  // //   Serial.print(";");
 
-    // Serial.print("Turning on pin ");
-    // Serial.print(scale_pins[i]);
-    // Serial.print(";");
+  // // //   // Serial.print("Turning on pin ");
+  // // //   // Serial.print(scale_pins[i]);
+  // // //   // Serial.print(";");
 
-    // // open mosfet switch, turning scale on
-    // digitalWrite(scale_pins[i], LOW);
-    // delay(timer);
+  // //   // open mosfet switch, turning scale on
+  // //   digitalWrite(scale_pins[i], LOW);
+  // //   delay(timer);
     
 
-    Serial.print(scale3.get_units(3), 1); //scale.get_units() returns a float
-    Serial.print(";");
+  // //   Serial.print(scales[i].get_units(3), 1); //scale.get_units() returns a float
+  // //   Serial.print(";");
 
-    // // close mosfet switch, turning scale off.
-    // Serial.print("Turning off pin ");
-    // Serial.print(scale_pins[i]);
+  // //   // // close mosfet switch, turning scale off.
+  // //   // Serial.print("Turning off pin ");
+  // //   // Serial.print(scale_pins[i]);
 
-    // digitalWrite(scale_pins[i], HIGH);
-    // delay(timer);
-    i++;
-
-  }
+  // //   digitalWrite(scale_pins[i], HIGH);
+  // //   delay(timer);
+  //   i++;
+  //   Serial.print(i);
+  // }
 //////////////////////////////////////////////////////////////////////////////////////////
-  // pinMode(7, OUTPUT);     
-  // digitalWrite(7, LOW);
-
-  // Serial.print(scale3.get_units(3), 1); //scale.get_units() returns a float
-  // Serial.print(";");
-
-  // Serial.print(scale2.get_units(3), 1);
-  // Serial.print(";");
+  // Turn on pin 7, opening mosfet, collect data, turn off pin, close mosfet
+  digitalWrite(7, LOW);
+  delay(timer);
+  Serial.print(scale3.get_units(3), 1); //scale.get_units() returns a float
+  Serial.print(";");
+  digitalWrite(7, HIGH);
+  delay(timer);
+  
+  // Turn on pin 8, opening mosfet, collect data, turn off pin, close mosfet
+  digitalWrite(8, LOW);
+  delay(timer);
+  Serial.print(scale4.get_units(3), 1);
+  Serial.print(";");
+  digitalWrite(8, HIGH);
+  delay(timer);
 
   Serial.println(Runs);
 
   delay(1000);
   Runs=Runs+1;
   }
+
+  // Helper function to get the scale factor for each scale based on its index
+float getScaleFactor(int index) {
+  switch (index) {
+    case 0:
+      return -470.98784;
+    case 1:
+      return -493.1443;
+    case 2:
+      return 466.98718;
+    case 3:
+      return 498.09172;
+    default:
+      return 1.0;  // Default scale factor if index is out of range
+  }
+}
