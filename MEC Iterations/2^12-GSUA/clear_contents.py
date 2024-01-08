64b9cf9718a9c44a3a8b31f229cb46c040328ebb
donaldscoon <46572318+donaldscoon@users.noreply.github.com>
2024-01-08 14:54:08 -0500

    GSUA completed.

---
+++ MEC Iterations/2^12-GSUA/clear_contents.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import messagebox
+import naming_function
+
+gen_path, indiv_path, structure_path = naming_function.path_names()
+
+
+def erase_files_in_folder(folder_path):
+    try:
+        if not os.path.exists(folder_path):
+            messagebox.showinfo("Folder Not Found", f"The folder '{folder_path}' does not exist.")
+            return
+        
+        confirmation = messagebox.askyesno("Confirmation", f"Are you sure you want to erase all files within '{folder_path}'?")
+        
+        if confirmation:
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    os.remove(file_path)
+
+            messagebox.showinfo("Deletion Complete", f"All files within '{folder_path}' have been erased.")
+        else:
+            messagebox.showinfo("Deletion Cancelled", "Deletion process cancelled.")
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
+
+def confirm_and_erase(folder_path):
+    erase_files_in_folder(folder_path)
+
+def clear_GSUA_sim_csv():
+    if os.path.exists(f'{structure_path}GSUA_simulations.csv'):
+        os.remove(f'{structure_path}GSUA_simulations.csv')
+
